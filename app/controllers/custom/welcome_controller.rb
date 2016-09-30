883e6d7898163a0fdabb45b4e9444e9817a15ad6
Ignacio Aliende García <ialiendeg@gmail.com>
2016-09-30 16:32:55 +0100

    Filter home page listings by confidence score and recover comments link in proposals and debates

--- app/controllers/custom/welcome_controller.rb
+++ app/controllers/custom/welcome_controller.rb
@@ -1,28 +1,20 @@
-class WelcomeController < ApplicationController
-  include CommentableActions
-  skip_authorization_check
+require_dependency Rails.root.join('app', 'controllers', 'welcome_controller').to_s
 
-  layout "devise", only: [:welcome, :verification]
+class WelcomeController
+  include CommentableActions
 
   def index
     if current_user
       redirect_to :proposals
     end
+
     @proposals = Proposal.all.not_archived
-    @proposals = @proposals.page(1).limit(2).for_render.send("sort_by_hot_score")
+    @proposals = @proposals.page(1).limit(2).for_render.send("sort_by_confidence_score")
     set_proposal_votes(@proposals)
 
     @debates = Debate.all
-    @debates = @debates.page(1).limit(2).for_render.send("sort_by_hot_score")
+    @debates = @debates.page(1).limit(2).for_render.send("sort_by_confidence_score")
     set_debate_votes(@debates)
   end
-
-  def welcome
-  end
-
-  def verification
-    redirect_to verification_path if signed_in?
-  end
-
 
 end
