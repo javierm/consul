f467b99edb4b5a2ced2c9d58970eb617ec40a167
Ignacio Aliende García <ialiendeg@gmail.com>
2016-12-16 13:16:55 +0000

    Removes faker gem from demo_seed

--- db/demo_seeds.rb
+++ db/demo_seeds.rb
@@ -1,4 +1,3 @@
-require 'database_cleaner'
 require 'csv'
 
 csv_text = File.read(Rails.root.join('db', 'demo_users.csv'))
@@ -7,7 +6,7 @@ csv = CSV.parse(csv_text, :headers => false, col_sep: ';')
 
 puts "Creating Users"
 
-def create_user(email, username = Faker::Name.name)
+def create_user(email, username = nil)
   pwd = '12345678'
   puts "    #{username}"
   user = User.where(username: username, email: email).first_or_initialize
@@ -24,6 +23,5 @@ end
 csv.each_with_index do |row, i|
     puts row
     a = create_user(row.last, row.first)
-    a.update(residence_verified_at: Time.now, confirmed_phone: Faker::PhoneNumber.phone_number, document_type: "1", verified_at: Time.now, document_number:i.to_s*8 )
+    a.update(residence_verified_at: Time.now, confirmed_phone: "9#{i.to_s*8}", document_type: "1", verified_at: Time.now, document_number:i.to_s*8 )
 end
-
