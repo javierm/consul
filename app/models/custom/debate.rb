2dae415c0d2b47fdcf949d496e32aec81f72e639
Ignacio Aliende García <ialiendeg@gmail.com>
2017-07-17 10:49:15 +0100

    Fixes thematic area save on create (adds default blank value)

--- app/models/custom/debate.rb
+++ app/models/custom/debate.rb
@@ -2,10 +2,4 @@ require_dependency Rails.root.join('app', 'models', 'debate').to_s
 
 class Debate
   validates :area, presence: true
-  before_validation :set_default_area, if: :new_record?
-
-  private
-  def set_default_area
-    self.area = I18n.t('debates.edit.area').values.first
-  end
 end
