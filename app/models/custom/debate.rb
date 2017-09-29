0b7e055ca55dd811137d9ffb93391611ce140069
Ignacio Aliende García <ialiendeg@gmail.com>
2017-09-29 12:51:45 +0100

    Limits debate votes to verified users

--- app/models/custom/debate.rb
+++ app/models/custom/debate.rb
@@ -2,4 +2,9 @@ require_dependency Rails.root.join('app', 'models', 'debate').to_s
 
 class Debate
   validates :area, presence: true
+
+  def votable_by?(user)
+    return false unless user.level_two_or_three_verified?
+    super(user)
+  end
 end
