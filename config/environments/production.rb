cd2fd75e2413ca7ed6e6cc1a56e2c6999b5f783b
Ignacio Aliende García <ialiendeg@gmail.com>
2017-10-11 11:41:37 +0100

    Adds from email to config/environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = { from: Rails.application.secrets.noreply }
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
 
