57286fc39e8419ada66d3aadfa0354657152f2fb
Ignacio Aliende García <ialiendeg@gmail.com>
2017-10-19 09:12:34 +0100

    Opens web

--- config/routes.rb
+++ config/routes.rb
@@ -1,275 +1,275 @@
 Rails.application.routes.draw do
 
-  # devise_for :users, controllers: {
-  #                      registrations: 'users/registrations',
-  #                      sessions: 'users/sessions',
-  #                      confirmations: 'users/confirmations',
-  #                      omniauth_callbacks: 'users/omniauth_callbacks'
-  #                    }
-  # devise_for :organizations, class_name: 'User',
-  #            controllers: {
-  #              registrations: 'organizations/registrations',
-  #              sessions: 'devise/sessions',
-  #            },
-  #            skip: [:omniauth_callbacks]
-  #
-  # devise_scope :organization do
-  #   get 'organizations/sign_up/success', to: 'organizations/registrations#success'
-  # end
-  #
-  # devise_scope :user do
-  #   patch '/user/confirmation', to: 'users/confirmations#update', as: :update_user_confirmation
-  #   get '/user/registrations/check_username', to: 'users/registrations#check_username'
-  #   get 'users/sign_up/success', to: 'users/registrations#success'
-  #   get 'users/registrations/delete_form', to: 'users/registrations#delete_form'
-  #   delete 'users/registrations', to: 'users/registrations#delete'
-  #   get :finish_signup, to: 'users/registrations#finish_signup'
-  #   patch :do_finish_signup, to: 'users/registrations#do_finish_signup'
-  # end
+  devise_for :users, controllers: {
+                       registrations: 'users/registrations',
+                       sessions: 'users/sessions',
+                       confirmations: 'users/confirmations',
+                       omniauth_callbacks: 'users/omniauth_callbacks'
+                     }
+  devise_for :organizations, class_name: 'User',
+             controllers: {
+               registrations: 'organizations/registrations',
+               sessions: 'devise/sessions',
+             },
+             skip: [:omniauth_callbacks]
 
-  root 'welcome#closed'
-  # get '/welcome', to: 'welcome#welcome'
-  # get '/cuentasegura', to: 'welcome#verification', as: :cuentasegura
-  # get '/resume', to: 'welcome#resume', as: :resume
-  #
-  # resources :debates do
-  #   member do
-  #     post :vote
-  #     put :flag
-  #     put :unflag
-  #     put :mark_featured
-  #     put :unmark_featured
-  #   end
-  #   collection do
-  #     get :map
-  #     get :suggest
-  #   end
-  # end
-  #
-  # resources :proposals do
-  #   member do
-  #     post :vote
-  #     post :vote_featured
-  #     put :flag
-  #     put :unflag
-  #     get :retire_form
-  #     patch :retire
-  #   end
-  #   collection do
-  #     get :map
-  #     get :suggest
-  #     get :summary
-  #   end
-  # end
-  #
-  # resources :proposal_ballots, only: [:index]
-  #
-  # resources :comments, only: [:create, :show], shallow: true do
-  #   member do
-  #     post :vote
-  #     put :flag
-  #     put :unflag
-  #   end
-  # end
-  #
-  # scope '/participatory_budget' do
-  #   resources :spending_proposals, only: [:index, :new, :create, :show, :destroy], path: 'investment_projects' do
-  #     post :vote, on: :member
-  #   end
-  # end
-  #
-  # resources :stats, only: [:index]
-  #
-  # resources :legislations, only: [:show]
-  #
-  # resources :annotations do
-  #   get :search, on: :collection
-  # end
-  #
-  # resources :users, only: [:show] do
-  #   resources :direct_messages, only: [:new, :create, :show]
-  # end
-  #
-  # resource :account, controller: "account", only: [:show, :update, :delete] do
-  #   get :erase, on: :collection
-  # end
-  #
-  # resources :notifications, only: [:index, :show] do
-  #   put :mark_all_as_read, on: :collection
-  # end
-  #
-  # resources :proposal_notifications, only: [:new, :create, :show]
-  #
-  # resource :verification, controller: "verification", only: [:show]
-  #
-  # resources :surveys, only: [:show] do
-  #   resources :answered_surveys, only: [:new, :create, :show]
-  # end
-  #
-  # scope module: :verification do
-  #   resource :residence, controller: "residence", only: [:new, :create]
-  #   resource :sms, controller: "sms", only: [:new, :create, :edit, :update]
-  #   resource :verified_user, controller: "verified_user", only: [:show]
-  #   resource :email, controller: "email", only: [:new, :show, :create]
-  #   resource :letter, controller: "letter", only: [:new, :create, :show, :edit, :update]
-  # end
-  #
-  # namespace :admin do
-  #   root to: "dashboard#index"
-  #   resources :organizations, only: :index do
-  #     get :search, on: :collection
-  #     member do
-  #       put :verify
-  #       put :reject
-  #     end
-  #   end
-  #
-  #   resources :users, only: [:index, :show] do
-  #     member do
-  #       put :restore
-  #       put :confirm_hide
-  #     end
-  #   end
-  #
-  #   resources :debates, only: :index do
-  #     member do
-  #       put :restore
-  #       put :confirm_hide
-  #     end
-  #   end
-  #
-  #   resources :proposals, only: :index do
-  #     member do
-  #       put :restore
-  #       put :confirm_hide
-  #     end
-  #   end
-  #
-  #   resources :spending_proposals, only: [:index, :show, :edit, :update] do
-  #     member do
-  #       patch :assign_admin
-  #       patch :assign_valuators
-  #     end
-  #
-  #     get :summary, on: :collection
-  #   end
-  #
-  #   resources :banners, only: [:index, :new, :create, :edit, :update, :destroy] do
-  #     collection { get :search}
-  #   end
-  #
-  #   resources :comments, only: :index do
-  #     member do
-  #       put :restore
-  #       put :confirm_hide
-  #     end
-  #   end
-  #
-  #   resources :tags, only: [:index, :create, :update, :destroy]
-  #   resources :officials, only: [:index, :edit, :update, :destroy] do
-  #     get :search, on: :collection
-  #   end
-  #
-  #   resources :settings, only: [:index, :update]
-  #   resources :moderators, only: [:index, :create, :destroy] do
-  #     get :search, on: :collection
-  #   end
-  #
-  #   resources :valuators, only: [:index, :create] do
-  #     get :search, on: :collection
-  #     get :summary, on: :collection
-  #   end
-  #
-  #   resources :managers, only: [:index, :create, :destroy] do
-  #     get :search, on: :collection
-  #   end
-  #
-  #   resources :verifications, controller: :verifications, only: :index do
-  #     get :search, on: :collection
-  #     put :request_verification, on: :collection
-  #   end
-  #
-  #   resource :activity, controller: :activity, only: :show
-  #   resource :stats, only: :show do
-  #     get :proposal_notifications, on: :collection
-  #     get :direct_messages, on: :collection
-  #   end
-  #
-  #   namespace :api do
-  #     resource :stats, only: :show
-  #   end
-  # end
-  #
-  # namespace :moderation do
-  #   root to: "dashboard#index"
-  #
-  #   resources :users, only: :index do
-  #     member do
-  #       put :hide
-  #       put :hide_in_moderation_screen
-  #     end
-  #   end
-  #
-  #   resources :debates, only: :index do
-  #     put :hide, on: :member
-  #     put :moderate, on: :collection
-  #   end
-  #
-  #   resources :proposals, only: :index do
-  #     put :hide, on: :member
-  #     put :moderate, on: :collection
-  #   end
-  #
-  #   resources :comments, only: :index do
-  #     put :hide, on: :member
-  #     put :moderate, on: :collection
-  #   end
-  # end
-  #
-  # namespace :valuation do
-  #   root to: "spending_proposals#index"
-  #
-  #   resources :spending_proposals, only: [:index, :show, :edit] do
-  #     patch :valuate, on: :member
-  #   end
-  # end
-  #
-  # namespace :management do
-  #   root to: "dashboard#index"
-  #
-  #   resources :document_verifications, only: [:index, :new, :create] do
-  #     post :check, on: :collection
-  #   end
-  #
-  #   resources :email_verifications, only: [:new, :create]
-  #
-  #   resources :user_invites, only: [:new, :create]
-  #
-  #   resources :users, only: [:new, :create] do
-  #     collection do
-  #       delete :logout
-  #       delete :erase
-  #     end
-  #   end
-  #
-  #   resource :account, controller: "account", only: [:show]
-  #
-  #   get 'sign_in', to: 'sessions#create', as: :sign_in
-  #
-  #   resource :session, only: [:create, :destroy]
-  #   resources :proposals, only: [:index, :new, :create, :show] do
-  #     post :vote, on: :member
-  #     get :print, on: :collection
-  #   end
-  #   resources :debates, only: [] do
-  #     get :print, on: :collection
-  #   end
-  #
-  #   resources :spending_proposals, only: [:index, :new, :create, :show] do
-  #     post :vote, on: :member
-  #     get :print, on: :collection
-  #   end
-  # end
+  devise_scope :organization do
+    get 'organizations/sign_up/success', to: 'organizations/registrations#success'
+  end
+
+  devise_scope :user do
+    patch '/user/confirmation', to: 'users/confirmations#update', as: :update_user_confirmation
+    get '/user/registrations/check_username', to: 'users/registrations#check_username'
+    get 'users/sign_up/success', to: 'users/registrations#success'
+    get 'users/registrations/delete_form', to: 'users/registrations#delete_form'
+    delete 'users/registrations', to: 'users/registrations#delete'
+    get :finish_signup, to: 'users/registrations#finish_signup'
+    patch :do_finish_signup, to: 'users/registrations#do_finish_signup'
+  end
+
+  root 'welcome#index'
+  get '/welcome', to: 'welcome#welcome'
+  get '/cuentasegura', to: 'welcome#verification', as: :cuentasegura
+  get '/resume', to: 'welcome#resume', as: :resume
+
+  resources :debates do
+    member do
+      post :vote
+      put :flag
+      put :unflag
+      put :mark_featured
+      put :unmark_featured
+    end
+    collection do
+      get :map
+      get :suggest
+    end
+  end
+
+  resources :proposals do
+    member do
+      post :vote
+      post :vote_featured
+      put :flag
+      put :unflag
+      get :retire_form
+      patch :retire
+    end
+    collection do
+      get :map
+      get :suggest
+      get :summary
+    end
+  end
+
+  resources :proposal_ballots, only: [:index]
+
+  resources :comments, only: [:create, :show], shallow: true do
+    member do
+      post :vote
+      put :flag
+      put :unflag
+    end
+  end
+
+  scope '/participatory_budget' do
+    resources :spending_proposals, only: [:index, :new, :create, :show, :destroy], path: 'investment_projects' do
+      post :vote, on: :member
+    end
+  end
+
+  resources :stats, only: [:index]
+
+  resources :legislations, only: [:show]
+
+  resources :annotations do
+    get :search, on: :collection
+  end
+
+  resources :users, only: [:show] do
+    resources :direct_messages, only: [:new, :create, :show]
+  end
+
+  resource :account, controller: "account", only: [:show, :update, :delete] do
+    get :erase, on: :collection
+  end
+
+  resources :notifications, only: [:index, :show] do
+    put :mark_all_as_read, on: :collection
+  end
+
+  resources :proposal_notifications, only: [:new, :create, :show]
+
+  resource :verification, controller: "verification", only: [:show]
+
+  resources :surveys, only: [:show] do
+    resources :answered_surveys, only: [:new, :create, :show]
+  end
+
+  scope module: :verification do
+    resource :residence, controller: "residence", only: [:new, :create]
+    resource :sms, controller: "sms", only: [:new, :create, :edit, :update]
+    resource :verified_user, controller: "verified_user", only: [:show]
+    resource :email, controller: "email", only: [:new, :show, :create]
+    resource :letter, controller: "letter", only: [:new, :create, :show, :edit, :update]
+  end
+
+  namespace :admin do
+    root to: "dashboard#index"
+    resources :organizations, only: :index do
+      get :search, on: :collection
+      member do
+        put :verify
+        put :reject
+      end
+    end
+
+    resources :users, only: [:index, :show] do
+      member do
+        put :restore
+        put :confirm_hide
+      end
+    end
+
+    resources :debates, only: :index do
+      member do
+        put :restore
+        put :confirm_hide
+      end
+    end
+
+    resources :proposals, only: :index do
+      member do
+        put :restore
+        put :confirm_hide
+      end
+    end
+
+    resources :spending_proposals, only: [:index, :show, :edit, :update] do
+      member do
+        patch :assign_admin
+        patch :assign_valuators
+      end
+
+      get :summary, on: :collection
+    end
+
+    resources :banners, only: [:index, :new, :create, :edit, :update, :destroy] do
+      collection { get :search}
+    end
+
+    resources :comments, only: :index do
+      member do
+        put :restore
+        put :confirm_hide
+      end
+    end
+
+    resources :tags, only: [:index, :create, :update, :destroy]
+    resources :officials, only: [:index, :edit, :update, :destroy] do
+      get :search, on: :collection
+    end
+
+    resources :settings, only: [:index, :update]
+    resources :moderators, only: [:index, :create, :destroy] do
+      get :search, on: :collection
+    end
+
+    resources :valuators, only: [:index, :create] do
+      get :search, on: :collection
+      get :summary, on: :collection
+    end
+
+    resources :managers, only: [:index, :create, :destroy] do
+      get :search, on: :collection
+    end
+
+    resources :verifications, controller: :verifications, only: :index do
+      get :search, on: :collection
+      put :request_verification, on: :collection
+    end
+
+    resource :activity, controller: :activity, only: :show
+    resource :stats, only: :show do
+      get :proposal_notifications, on: :collection
+      get :direct_messages, on: :collection
+    end
+
+    namespace :api do
+      resource :stats, only: :show
+    end
+  end
+
+  namespace :moderation do
+    root to: "dashboard#index"
+
+    resources :users, only: :index do
+      member do
+        put :hide
+        put :hide_in_moderation_screen
+      end
+    end
+
+    resources :debates, only: :index do
+      put :hide, on: :member
+      put :moderate, on: :collection
+    end
+
+    resources :proposals, only: :index do
+      put :hide, on: :member
+      put :moderate, on: :collection
+    end
+
+    resources :comments, only: :index do
+      put :hide, on: :member
+      put :moderate, on: :collection
+    end
+  end
+
+  namespace :valuation do
+    root to: "spending_proposals#index"
+
+    resources :spending_proposals, only: [:index, :show, :edit] do
+      patch :valuate, on: :member
+    end
+  end
+
+  namespace :management do
+    root to: "dashboard#index"
+
+    resources :document_verifications, only: [:index, :new, :create] do
+      post :check, on: :collection
+    end
+
+    resources :email_verifications, only: [:new, :create]
+
+    resources :user_invites, only: [:new, :create]
+
+    resources :users, only: [:new, :create] do
+      collection do
+        delete :logout
+        delete :erase
+      end
+    end
+
+    resource :account, controller: "account", only: [:show]
+
+    get 'sign_in', to: 'sessions#create', as: :sign_in
+
+    resource :session, only: [:create, :destroy]
+    resources :proposals, only: [:index, :new, :create, :show] do
+      post :vote, on: :member
+      get :print, on: :collection
+    end
+    resources :debates, only: [] do
+      get :print, on: :collection
+    end
+
+    resources :spending_proposals, only: [:index, :new, :create, :show] do
+      post :vote, on: :member
+      get :print, on: :collection
+    end
+  end
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
