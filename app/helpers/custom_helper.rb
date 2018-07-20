95955830da6ae57d93ed7f19aa91690e73abf6c6
Laura Concepción <lconcepcion@usabi.es>
2018-07-20 16:28:32 +0100

    create new helper to show html in custom pages
    Signed-off-by: Laura Concepción <lconcepcion@usabi.es>

--- app/helpers/custom_helper.rb
+++ app/helpers/custom_helper.rb
@@ -6,4 +6,14 @@ module CustomHelper
   def proposal_area_options
     Proposal::AREAS.map {|option| [ t("proposals.edit.area.#{option}"), option ] }
   end
+
+  ALLOWED_TAGS = %w(p ul ol li strong em u s iframe img)
+  ALLOWED_ATTRIBUTES =  %w(class style href width height src alt)
+
+  def text_in_html(text)
+    return unless text
+    sanitized = sanitize(text, tags: ALLOWED_TAGS, attributes: ALLOWED_ATTRIBUTES)
+    Rinku.auto_link(sanitized, :all, 'target="_blank" rel="nofollow"').html_safe
+  end
+
 end
