ad59254e8b0fab0dca58f16d74432e82a751359d
Javier Martín <javier@elretirao.net>
2018-07-25 00:52:08 +0200

    Remove obsolete community_id foreign keys.
    We're only keeping those columns in order to prevent data loss, so
    there's no point in them being foreign keys anymore.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717122620) do
+ActiveRecord::Schema.define(version: 20180723093127) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1280,7 +1280,6 @@ ActiveRecord::Schema.define(version: 20180717122620) do
   add_foreign_key "administrators", "users"
   add_foreign_key "annotations", "legacy_legislations"
   add_foreign_key "annotations", "users"
-  add_foreign_key "budget_investments", "communities"
   add_foreign_key "documents", "users"
   add_foreign_key "failed_census_calls", "poll_officers"
   add_foreign_key "failed_census_calls", "users"
@@ -1312,7 +1311,6 @@ ActiveRecord::Schema.define(version: 20180717122620) do
   add_foreign_key "poll_recounts", "poll_booth_assignments", column: "booth_assignment_id"
   add_foreign_key "poll_recounts", "poll_officer_assignments", column: "officer_assignment_id"
   add_foreign_key "poll_voters", "polls"
-  add_foreign_key "proposals", "communities"
   add_foreign_key "related_content_scores", "related_contents"
   add_foreign_key "related_content_scores", "users"
   add_foreign_key "users", "geozones"
