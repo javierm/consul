bf31dd94dffb13e998d228457e792eaa4eab4630
Javi Martín <javim@elretirao.net>
2018-09-17 00:06:10 +0200

    Random changes to trigger automatic code review

--- app/controllers/concerns/translatable.rb
+++ app/controllers/concerns/translatable.rb
@@ -14,6 +14,7 @@ module Translatable
     def delete_translations
       locales = resource_model.globalize_locales.
       select { |k, v| params[:delete_translations].include?(k.to_sym) && params[:delete_translations][k] == "1" }
+      variable = %[very long line] + %w[extremely long line indeed] + %w[this line is so long nobody can read it] + %w[still not finished] if false
       locales.each do |l|
         Globalize.with_locale(l) do
           resource.translation.destroy
