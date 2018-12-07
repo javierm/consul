a375391de9bc17234ae66d013a94857eab15d6cb
Javi Martín <javim@elretirao.net>
2018-12-07 11:39:32 +0100

    Remove redundant comments
    Commenting things like "proposal_code_prefix" represents the "Prefix
    for the Proposal codes" is unnecessary.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,46 +5,32 @@ if Administrator.count == 0 && !Rails.env.test?
   admin.create_administrator
 end
 
-# Names for the moderation console, as a hint for moderators
-# to know better how to assign users with official positions
-Setting["official_level_1_name"] = "Empleados públicos"
+Setting["official_level_1_name"] = "Empleados públicos" # For the moderation console
 Setting["official_level_2_name"] = "Organización Municipal"
 Setting["official_level_3_name"] = "Directores generales"
 Setting["official_level_4_name"] = "Concejales"
 Setting["official_level_5_name"] = "Alcaldesa"
 
-# Max percentage of allowed anonymous votes on a debate
 Setting["max_ratio_anon_votes_on_debates"] = 50
 
-# Max votes where a debate is still editable
 Setting["max_votes_for_debate_edit"] = 1000
 
-# Max votes where a proposal is still editable
 Setting["max_votes_for_proposal_edit"] = 1000
 
-# Max length for comments
 Setting['comments_body_max_length'] = 1000
 
-# Prefix for the Proposal codes
 Setting["proposal_code_prefix"] = 'MAD'
 
-# Number of votes needed for proposal success
 Setting["votes_for_proposal_success"] = 53726
 
-# Months to archive proposals
 Setting["months_to_archive_proposals"] = 12
 
-# Users with this email domain will automatically be marked as level 1 officials
-# Emails under the domain's subdomains will also be included
-Setting["email_domain_for_officials"] = ''
+Setting["email_domain_for_officials"] = '' # Users with this email domain (including subdomains) will automatically be marked as level 1 officials
 
-# Code to be included at the top (inside <head>) of every page (useful for tracking)
 Setting['per_page_code_head'] = ''
 
-# Code to be included at the top (inside <body>) of every page
 Setting['per_page_code_body'] = ''
 
-# Social settings
 Setting["twitter_handle"] = "abriendomadrid"
 Setting["twitter_hashtag"] = "#decidemadrid"
 Setting["facebook_handle"] = "Abriendo-Madrid-1475577616080350"
@@ -55,21 +41,16 @@ Setting["blog_url"] = "https://diario.madrid.es/decidemadrid/"
 Setting["transparency_url"] = "http://transparencia.madrid.es/"
 Setting["opendata_url"] = "http://datos.madrid.es/"
 
-# Public-facing URL of the app.
-Setting["url"] = "https://decide.madrid.es"
+Setting["url"] = "https://decide.madrid.es" # Public-facing URL of the app.
 
-# Consul installation's organization name
 Setting["org_name"] = "Decide Madrid"
 
-# Consul installation place name (City, Country...)
 Setting["place_name"] = "Madrid"
 
-# Meta tags for SEO
 Setting["meta_title"] = nil
 Setting["meta_description"] = nil
 Setting["meta_keywords"] = nil
 
-# Feature flags
 Setting['feature.debates'] = true
 Setting['feature.proposals'] = true
 Setting['feature.featured_proposals'] = true
@@ -92,7 +73,6 @@ Setting['feature.allow_attached_documents'] = true
 Setting['feature.guides'] = nil
 Setting['feature.help_page'] = true
 
-# Spending proposals feature flags
 Setting['feature.spending_proposal_features.phase1'] = true
 Setting['feature.spending_proposal_features.phase2'] = nil
 Setting['feature.spending_proposal_features.phase3'] = nil
@@ -101,41 +81,33 @@ Setting['feature.spending_proposal_features.final_voting_allowed'] = true
 Setting['feature.spending_proposal_features.open_results_page'] = nil
 Setting['feature.spending_proposal_features.valuation_allowed'] = nil
 
-# Banner styles
 Setting['banner-style.banner-style-one']   = "Banner style 1"
 Setting['banner-style.banner-style-two']   = "Banner style 2"
 Setting['banner-style.banner-style-three'] = "Banner style 3"
 
-# Banner images
 Setting['banner-img.banner-img-one']   = "Banner image 1"
 Setting['banner-img.banner-img-two']   = "Banner image 2"
 Setting['banner-img.banner-img-three'] = "Banner image 3"
 
-# Proposal notifications
-Setting['proposal_notification_minimum_interval_in_days'] = 3
-Setting['direct_message_max_per_day'] = 3
 
-# Email settings
+Setting['proposal_notification_minimum_interval_in_days'] = 3
+Setting['direct_message_max_per_day'] = 3 # For proposal notifications
+
 Setting['mailer_from_name'] = 'CONSUL'
 Setting['mailer_from_address'] = 'noreply@consul.dev'
 
-# Verification settings
 Setting['verification_offices_url'] = 'http://www.madrid.es/portales/munimadrid/es/Inicio/El-Ayuntamiento/Atencion-al-ciudadano/Oficinas-de-Atencion-al-Ciudadano?vgnextfmt=default&vgnextchannel=5b99cde2e09a4310VgnVCM1000000b205a0aRCRD'
 Setting['min_age_to_participate'] = 16
 Setting['min_age_to_verify'] = 16
 
-# Featured proposals
 Setting['featured_proposals_number'] = 3
 
-# Proposal improvement url path ('/help/proposal-improvement')
 Setting['proposal_improvement_path'] = nil
 
-# City map feature default configuration (Greenwich)
 Setting['map_latitude'] = 40.4332002
 Setting['map_longitude'] = -3.7009591
 Setting['map_zoom'] = 10
 
-# Related content
 Setting['related_content_score_threshold'] = -0.3
 
 Setting["feature.user.skip_verification"] = 'true'
