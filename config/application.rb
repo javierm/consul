0e323217e8a52a307eeab6d551363676b89c7ec5
Javi Martín <javim@elretirao.net>
2019-07-06 17:33:11 +0200

    Add cells

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,7 @@ module Consul
 
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "cells")
 
     # Add lib to the autoload path
     config.autoload_paths << Rails.root.join("lib")
