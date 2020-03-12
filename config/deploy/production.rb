1027151d9f75957b13f19a5666b8c77b8a1a269e
Ignacio Aliende García <ialiendeg@gmail.com>
2020-03-12 12:44:36 +0000

    Updates capistrano production config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,7 @@
 set :branch, ENV["branch"] || :master
 
-server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
+server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
+# server deploysecret(:server1), user: deploysecret(:user), roles: %w[web app db importer cron background]
 #server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
 #server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
 #server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
