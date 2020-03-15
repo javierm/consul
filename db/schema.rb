609daf85e63b7ad6a1cd444c72587cc97dd9b338
Julian Herrero <microweb10@gmail.com>
2020-03-15 12:51:52 +0700

    Changes on participatory budgets
    - Add main button to budgets
    - Ability to attach an image to budgets
    - Add main button to each phase of the budget
    - Ability to edit the name of the budget phases
    - Ability to attach an image to budget phases
    - Ability to create single heading budgets step by step
    - UX improvements on the participatory budgets (public and admin)

--- db/schema.rb
+++ db/schema.rb
@@ -287,6 +287,7 @@ ActiveRecord::Schema.define(version: 20200519120717) do
     t.datetime "updated_at", null: false
     t.text "description"
     t.text "summary"
+    t.string "name"
     t.index ["budget_phase_id"], name: "index_budget_phase_translations_on_budget_phase_id"
     t.index ["locale"], name: "index_budget_phase_translations_on_locale"
   end
@@ -298,6 +299,8 @@ ActiveRecord::Schema.define(version: 20200519120717) do
     t.datetime "starts_at"
     t.datetime "ends_at"
     t.boolean "enabled", default: true
+    t.string "main_button_text"
+    t.string "main_button_url"
     t.index ["ends_at"], name: "index_budget_phases_on_ends_at"
     t.index ["kind"], name: "index_budget_phases_on_kind"
     t.index ["next_phase_id"], name: "index_budget_phases_on_next_phase_id"
@@ -361,6 +364,8 @@ ActiveRecord::Schema.define(version: 20200519120717) do
     t.text "description_publishing_prices"
     t.text "description_informing"
     t.boolean "published"
+    t.string "main_button_text"
+    t.string "main_button_url"
   end
 
   create_table "campaigns", id: :serial, force: :cascade do |t|
