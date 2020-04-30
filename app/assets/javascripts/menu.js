fb12e3b0f564d61b3a1ffbcfbe363b729c812aa6
Javi Martín <javim@elretirao.net>
2020-04-30 16:19:04 +0200

    Get previous behavior

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -2,7 +2,7 @@
   "use strict";
   App.Menu = {
     initialize: function() {
-      $("a[href='#side_menu']").on("click", function(event) {
+      $("[href='#side_menu'], [href='#hide_side_menu']").on("click", function(event) {
         event.preventDefault();
 
         $("#side_menu").toggleClass("has-menu");
