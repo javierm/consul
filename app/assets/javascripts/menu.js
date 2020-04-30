05db4ef0d87285ed9f5ca3e019a15812f9dbadeb
Javi Martín <javim@elretirao.net>
2020-04-30 15:07:08 +0200

    Simplify JavaScript

--- app/assets/javascripts/menu.js
+++ app/assets/javascripts/menu.js
@@ -3,23 +3,9 @@
   App.Menu = {
     initialize: function() {
       $("a[href='#side_menu']").on("click", function(event) {
-        var parent, content, wrapper;
-
         event.preventDefault();
-        event.stopPropagation();
 
-        parent = $("#side_menu").parent().addClass("has-menu");
-        content = $("#side_menu").next();
-        wrapper = $("<div class='has-menu-wrapper'></div>");
-
-        wrapper.on("click", function(e) {
-          e.stopPropagation();
-
-          parent.removeClass("has-menu");
-          $(this).remove();
-        });
-
-        content.prepend(wrapper);
+        $("#side_menu").toggleClass("has-menu");
       });
     }
   };
