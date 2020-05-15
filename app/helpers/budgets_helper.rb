3f865198078ea7994ca9c8d72546380fcdf6413e
decabeza <alberto@decabeza.es>
2020-05-15 10:37:10 +0200

    Add supports info section on budgets index page

--- app/helpers/budgets_helper.rb
+++ app/helpers/budgets_helper.rb
@@ -99,4 +99,8 @@ module BudgetsHelper
       }
     end
   end
+
+  def budget_investments_total_supports(user)
+    Vote.where(votable_type: "Budget::Investment", voter_id: user.id).count
+  end
 end
