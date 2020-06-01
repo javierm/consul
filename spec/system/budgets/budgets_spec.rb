01169c0941ba56840b7513d86a9284c9ad91aad1
Javi Martín <javim@elretirao.net>
2020-06-01 14:21:06 +0200

    Update tests to Capybara 3.x

--- spec/system/budgets/budgets_spec.rb
+++ spec/system/budgets/budgets_spec.rb
@@ -113,8 +113,8 @@ describe "Budgets" do
       visit budgets_path
 
       within("#budget_info") do
-        expect(page).not_to have_link "#{heading.name} €1,000,000"
-        expect(page).to have_content "#{heading.name} €1,000,000"
+        expect(page).not_to have_link "#{heading.name}\n€1,000,000"
+        expect(page).to have_content "#{heading.name}\n€1,000,000"
 
         expect(page).not_to have_link("List of all investment projects")
         expect(page).not_to have_link("List of all unfeasible investment projects")
@@ -131,8 +131,8 @@ describe "Budgets" do
       visit budgets_path
 
       within("#budget_info") do
-        expect(page).not_to have_link "#{heading.name} €1,000,000"
-        expect(page).to have_content "#{heading.name} €1,000,000"
+        expect(page).not_to have_link "#{heading.name}\n€1,000,000"
+        expect(page).to have_content "#{heading.name}\n€1,000,000"
 
         expect(page).to have_link "List of all investment projects",
                                    href: budget_path(budget)
