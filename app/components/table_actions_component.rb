c229e7965bf3e4c4fb28ad63212b03a164d70b56
Javi Martín <javim@elretirao.net>
2020-06-09 00:47:41 +0200

    Move table actions partial to a component
    This partial was going to get too complex since in some places we've got
    different texts, different URLs or different confirmation messages.
    While we should probably try to be more consistent and that would make
    the partial work in most cases, there'll always be some exceptions, and
    using a partial (with, perhaps, some helper methods) will become messy
    really quickly.

---
+++ app/components/table_actions_component.rb
@@ -0,0 +1,8 @@
+class TableActionsComponent < ApplicationComponent
+  attr_reader :record, :actions
+
+  def initialize(record, actions: [:edit, :destroy])
+    @record = record
+    @actions = actions
+  end
+end
