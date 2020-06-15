f8c4d130dbd64938c4003c2feb51e2628870af0f
Ignacio Aliende García <ialiendeg@gmail.com>
2020-06-15 19:25:50 +0100

    First design version

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,4 +1,7 @@
 module Consul
   class Application < Rails::Application
+    config.autoload_paths.unshift(Rails.root.join('lib/custom'))
+    config.i18n.default_locale = :es
+    config.i18n.available_locales = ["es"]
   end
 end
