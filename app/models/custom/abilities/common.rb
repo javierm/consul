9e8074c529c72613dd7f5f38fd64a94a023a7b06
Laura Concepción <lconcepcion@usabi.es>
2020-10-20 14:45:20 +0100

    abilities to verified organizations to create, edit and destroy budget investment
    Signed-off-by: Laura Concepción <lconcepcion@usabi.es>

--- app/models/custom/abilities/common.rb
+++ app/models/custom/abilities/common.rb
@@ -83,6 +83,14 @@ module Abilities
         can :vote, Comment
       end
 
+      if user.organization && user.organization.verified?
+        can :create, Budget::Investment,               budget: { phase: "accepting" }
+        can :edit, Budget::Investment,                 budget: { phase: "accepting" }, author_id: user.id
+        can :update, Budget::Investment,               budget: { phase: "accepting" }, author_id: user.id
+        can :suggest, Budget::Investment,              budget: { phase: "accepting" }
+        can :destroy, Budget::Investment,              budget: { phase: ["accepting", "reviewing"] }, author_id: user.id
+      end
+
       if user.level_two_or_three_verified?
         can :vote, Proposal, &:published?
         can :vote_featured, Proposal
