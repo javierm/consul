05e82acfea27c2f05e7320cb7d6315cc38e66a39
Ignacio Aliende García <ialiendeg@gmail.com>
2020-11-08 20:08:52 +0000

    Adds deploy environment for gva compilation in office server and task for rsync compiled code in local folder

---
+++ config/deploy/production_gva.rb
@@ -0,0 +1,16 @@
+set :branch, ENV["branch"] || :master
+set :bundle_path, -> { release_path.join('vendor/bundle') }
+set :linked_dirs, %w[log tmp public/system]
+set :gva_svn_path, -> { "#{ENV['PWD']}_svn" }
+
+server deploysecret(:server), user: deploysecret(:user), roles: %w[web app db importer cron background]
+#server deploysecret(:server2), user: deploysecret(:user), roles: %w(web app db importer cron background)
+#server deploysecret(:server3), user: deploysecret(:user), roles: %w(web app db importer)
+#server deploysecret(:server4), user: deploysecret(:user), roles: %w(web app db importer)
+
+namespace :deploy do
+  # Uncomment this to automatically do the rsync of the office server compiled code to local folder
+  # (defined with gva_svn_path setting. By default ../<project_folder>_svn)
+  # Or launch the task with "cap production_gva deploy:gva_subversion_copy"
+  after :finished, "deploy:gva_subversion_copy"
+end
