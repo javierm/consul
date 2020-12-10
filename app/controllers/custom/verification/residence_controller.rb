bb3db917dff35f70cc241954b97ee2adb0e4c294
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2020-12-10 15:15:25 +0000

    Remove sms verification
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

---
+++ app/controllers/custom/verification/residence_controller.rb
@@ -0,0 +1,17 @@
+require_dependency Rails.root.join("app", "controllers", "verification", "residence_controller").to_s
+
+class Verification::ResidenceController
+  def create
+    @residence = Verification::Residence.new(residence_params.merge(user: current_user))
+    if @residence.save
+      # we don't want to use sms verification
+      current_user.update!(unconfirmed_phone: '-', confirmed_phone: '-')
+      ahoy.track(:level_2_user, user_id: current_user.id) rescue nil
+      current_user.update(verified_at: Time.current)
+
+      redirect_to verified_user_path, notice: t("verification.residence.create.flash.success")
+    else
+      render :new
+    end
+  end
+end
