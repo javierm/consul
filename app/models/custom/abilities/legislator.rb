87f350287f8e5a882351649877d97e0a84a10af6
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2020-12-17 13:19:59 +0000

    Add user_id to legislator ability
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

--- app/models/custom/abilities/legislator.rb
+++ app/models/custom/abilities/legislator.rb
@@ -4,23 +4,25 @@ module Abilities
 
     def initialize(user)
       can :create, Legislation::Proposal
-      can :show, Legislation::Proposal
+      can :show, Legislation::Proposal, user_id: user.id
       can :proposals, ::Legislation::Process
 
-      can :restore, Legislation::Proposal
+      can :restore, Legislation::Proposal, user_id: user.id
       cannot :restore, Legislation::Proposal, hidden_at: nil
 
-      can :confirm_hide, Legislation::Proposal
+      can :confirm_hide, Legislation::Proposal, user_id: user.id
       cannot :confirm_hide, Legislation::Proposal, hidden_at: nil
 
-      can :comment_as_administrator, [Legislation::Question, Legislation::Proposal, Legislation::Annotation]
+      can :comment_as_administrator, [Legislation::Question, Legislation::Annotation]
+      can :comment_as_administrator, [Legislation::Proposal], user_id: user.id
 
       can [:read, :debate, :draft_publication, :allegations, :result_publication,
-          :milestones], Legislation::Process
-      can [:create, :update, :destroy], Legislation::Process
+          :milestones], Legislation::Process, user_id: user.id
+      can [:create], Legislation::Process
+      can [:update, :destroy], Legislation::Process, user_id: user.id
       can [:manage], ::Legislation::DraftVersion
       can [:manage], ::Legislation::Question
-      can [:manage], ::Legislation::Proposal
+      can [:manage], ::Legislation::Proposal, user_id: user.id
       cannot :comment_as_moderator, [::Legislation::Question, Legislation::Annotation, ::Legislation::Proposal]
     end
   end
