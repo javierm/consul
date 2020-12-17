1855e966474db4dc5daebcdfad7c7730da508a8a
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2020-12-17 13:36:07 +0000

    Fix user_id references in legislator ability
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

--- app/models/custom/abilities/legislator.rb
+++ app/models/custom/abilities/legislator.rb
@@ -4,17 +4,16 @@ module Abilities
 
     def initialize(user)
       can :create, Legislation::Proposal
-      can :show, Legislation::Proposal, user_id: user.id
+      can :show, Legislation::Proposal
       can :proposals, ::Legislation::Process
 
-      can :restore, Legislation::Proposal, user_id: user.id
+      can :restore, Legislation::Proposal
       cannot :restore, Legislation::Proposal, hidden_at: nil
 
-      can :confirm_hide, Legislation::Proposal, user_id: user.id
+      can :confirm_hide, Legislation::Proposal
       cannot :confirm_hide, Legislation::Proposal, hidden_at: nil
 
-      can :comment_as_administrator, [Legislation::Question, Legislation::Annotation]
-      can :comment_as_administrator, [Legislation::Proposal], user_id: user.id
+      can :comment_as_administrator, [Legislation::Question, Legislation::Annotation, Legislation::Proposal]
 
       can [:read, :debate, :draft_publication, :allegations, :result_publication,
           :milestones], Legislation::Process, user_id: user.id
@@ -22,7 +21,7 @@ module Abilities
       can [:update, :destroy], Legislation::Process, user_id: user.id
       can [:manage], ::Legislation::DraftVersion
       can [:manage], ::Legislation::Question
-      can [:manage], ::Legislation::Proposal, user_id: user.id
+      can [:manage], ::Legislation::Proposal
       cannot :comment_as_moderator, [::Legislation::Question, Legislation::Annotation, ::Legislation::Proposal]
     end
   end
