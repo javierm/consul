63a8705d154b344935a8c83475fe35cecd5b764a
Javi Martín <javim@elretirao.net>
2021-03-26 19:38:06 +0100

    Admin menu

--- spec/system/admin/banners_spec.rb
+++ spec/system/admin/banners_spec.rb
@@ -64,6 +64,8 @@ describe "Admin banners magement", :admin do
     visit admin_root_path
 
     within("#side_menu") do
+      click_link "Site content"
+
       expect(page).to have_link "Manage banners"
     end
   end
@@ -180,6 +182,7 @@ describe "Admin banners magement", :admin do
     visit admin_root_path
 
     within("#side_menu") do
+      click_link "Site content"
       click_link "Manage banners"
     end
 
