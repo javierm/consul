7d8bf9e7ea090e5e14122bfd0130c3d570e18490
Adán Alonso Salvador <adan.alonso.s@gmail.com>
2021-04-26 15:17:02 +0100

    Finetune budget manager permissions
    Signed-off-by: Adán Alonso Salvador <adan.alonso.s@gmail.com>

--- app/models/custom/abilities/budget_manager.rb
+++ app/models/custom/abilities/budget_manager.rb
@@ -7,17 +7,15 @@ module Abilities
 
       can :comment_as_administrator, [Budget::Investment]
 
-      can [:index, :read, :new, :create, :update, :destroy, :calculate_winners], Budget
-      can [:read, :create, :update, :destroy], Budget::Group
-      can [:read, :create, :update, :destroy], Budget::Heading
+      can [:index, :read], Budget
+      can [:read], Budget::Group
+      can [:read], Budget::Heading
       can [:hide, :admin_update, :toggle_selection], Budget::Investment
       can [:valuate, :comment_valuation], Budget::Investment
       cannot [:admin_update, :toggle_selection, :valuate, :comment_valuation],
         Budget::Investment, budget: { phase: "finished" }
 
       can :create, Budget::ValuatorAssignment
-
-      can :read_admin_stats, Budget, &:balloting_or_later?
     end
   end
 end
