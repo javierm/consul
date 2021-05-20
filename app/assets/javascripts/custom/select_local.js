07ca104e39d7775c2f8795333fff964e7ec238e0
Ignacio Aliende García <ialiendeg@gmail.com>
2021-05-20 15:19:08 +0100

    Fixes language change because of turbolinks event

--- app/assets/javascripts/custom/select_local.js
+++ app/assets/javascripts/custom/select_local.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('turbolinks:load', function(event) {
   $("input[type=radio][name=select-local]").on("change", function () {
     window.location.assign($(this).val());
   });
