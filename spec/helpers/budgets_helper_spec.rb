46cde0ef2cd0752d6448e85ab95925c4b80bacba
Javi Martín <javim@elretirao.net>
2021-06-04 21:58:09 +0200

    Extract budget form to a component
    This way it will be easier to change it and reuse it.

--- spec/helpers/budgets_helper_spec.rb
+++ spec/helpers/budgets_helper_spec.rb
@@ -1,14 +1,4 @@
 require "rails_helper"
 
 describe BudgetsHelper do
-  describe "#budget_voting_styles_select_options" do
-    it "provides vote kinds" do
-      types = [
-        ["Knapsack", "knapsack"],
-        ["Approval", "approval"]
-      ]
-
-      expect(budget_voting_styles_select_options).to eq(types)
-    end
-  end
 end
