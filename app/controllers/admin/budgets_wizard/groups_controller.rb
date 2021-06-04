5e7c824967952caa4f3513f3a777b7d6c6af2aea
taitus <sebastia.roig@gmail.com>
2021-06-04 17:52:31 +0200

    Adapt group step to single and multiple budget mode
    Co-Authored-By: decabeza <alberto@decabeza.es>

--- app/controllers/admin/budgets_wizard/groups_controller.rb
+++ app/controllers/admin/budgets_wizard/groups_controller.rb
@@ -2,18 +2,35 @@ class Admin::BudgetsWizard::GroupsController < Admin::BaseController
   include Admin::BudgetGroupsActions
 
   before_action :load_groups, only: [:index, :create]
+  before_action :set_budget_mode
 
   def index
-    @group = @budget.groups.new
+    if @mode == "single"
+      @group = @budget.groups.new("name_#{I18n.locale}" => @budget.name)
+    else
+      @group = @budget.groups.new
+    end
   end
 
   private
 
     def groups_index
-      admin_budgets_wizard_budget_groups_path(@budget)
+      if @mode == "multiple"
+        admin_budgets_wizard_budget_groups_path(@budget, url_params)
+      else
+        admin_budgets_wizard_budget_group_headings_path(@budget, @group, url_params)
+      end
     end
 
     def new_action
       :index
+    end
+
+    def url_params
+      @mode.present? ? { mode: @mode } : {}
+    end
+
+    def set_budget_mode
+      @mode = params[:mode] || params.dig(:heading, :mode)
     end
 end
