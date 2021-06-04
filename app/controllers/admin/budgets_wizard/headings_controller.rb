95d571860cf2a04872633cf287acc0bd7c36e18a
taitus <sebastia.roig@gmail.com>
2021-06-04 18:23:36 +0200

    Adapt heading step to single and multiple budget mode
    Co-Authored-By: decabeza <alberto@decabeza.es>

--- app/controllers/admin/budgets_wizard/headings_controller.rb
+++ app/controllers/admin/budgets_wizard/headings_controller.rb
@@ -2,6 +2,7 @@ class Admin::BudgetsWizard::HeadingsController < Admin::BaseController
   include Admin::BudgetHeadingsActions
 
   before_action :load_headings, only: [:index, :create]
+  before_action :set_budget_mode
 
   def index
     @heading = @group.headings.new
@@ -10,10 +11,22 @@ class Admin::BudgetsWizard::HeadingsController < Admin::BaseController
   private
 
     def headings_index
-      admin_budgets_wizard_budget_group_headings_path(@budget, @group)
+      if @mode == "multiple"
+        admin_budgets_wizard_budget_group_headings_path(@budget, @group, url_params)
+      else
+        admin_budgets_wizard_budget_budget_phases_path(@budget, url_params)
+      end
     end
 
     def new_action
       :index
+    end
+
+    def url_params
+      @mode.present? ? { mode: @mode } : {}
+    end
+
+    def set_budget_mode
+      @mode = params[:mode] || params.dig(:budget, :mode)
     end
 end
