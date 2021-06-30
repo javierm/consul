0214889f44ae8f0d7c3ca02ca4ffea006a535259
Javi Martín <javim@elretirao.net>
2021-06-30 20:17:28 +0200

    Simplify language selection with a few languages
    As mentioned in commit 5214d89c8, using the `change` event of a `select`
    field to automatically change location is really annoying for keyboard
    users, since the event will trigger when pressing the down key to
    navigate through the options or when typing a key to start searching for
    an option. This might cause a lot of frustration.
    
    Most multilanguage CONSUL sites enable between 2 and 4 languages. In
    these cases, it's easier to just display the list of languages to
    simplify the selection.
    
    This way in this situation we also make it clear which languages are
    available. If we use a `<select>` tag, users will have to open it in
    order to check whether the site is available in their preferred
    language.
    
    This is also useful when the current language uses characters users
    don't recognize; users will recognize their own language in the list of
    available languages, while it might be harder to recognize the language
    selector allows them to switch to a different language.
    
    In this case, we're also hiding the label because a list of links with
    language names is usually self explanatory for sighted users. We're
    still providing it for screen reader users so they immediately know the
    list allows them to change the language and if they don't need to do so
    they can quickly skip it.

--- app/components/layout/locale_switcher_component.rb
+++ app/components/layout/locale_switcher_component.rb
@@ -7,8 +7,31 @@ class Layout::LocaleSwitcherComponent < ApplicationComponent
 
   private
 
+    def many_locales?
+      locales.size > 4
+    end
+
     def locales
       I18n.available_locales
+    end
+
+    def label
+      t("layouts.header.locale")
+    end
+
+    def label_id
+      "locale_switcher_label"
+    end
+
+    def language_items
+      safe_join(locales.map { |locale| tag.li { locale_link(locale) } })
+    end
+
+    def locale_link(locale)
+      link_to name_for_locale(locale),
+        current_path_with_query_params(locale: locale),
+        lang: locale,
+        "aria-current" => (true if locale == I18n.locale)
     end
 
     def language_options
