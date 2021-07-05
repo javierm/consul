e1bf3f0a29a0aa2ce45930f799c5cab50107a1ba
Ignacio Aliende García <ialiendeg@gmail.com>
2021-07-05 14:47:57 +0100

    Makes central section customizable

--- app/models/custom/widget/card.rb
+++ app/models/custom/widget/card.rb
@@ -4,4 +4,12 @@ class Widget::Card
   translates :link_url, touch: true
   translates :link_text_2, touch: true
   translates :link_url_2, touch: true
+
+  def self.body
+    where(header: false, middle: false, site_customization_page_id: nil).order(:created_at)
+  end
+
+  def self.middle
+    where(middle: true).order(:id)
+  end
 end
