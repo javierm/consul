10b394abd716080b9675e8dfbbdaa4fd51f809bf
Javi Martín <javim@elretirao.net>
2021-08-20 02:33:05 +0200

    Use a button toggle proposal selection
    As mentioned in previous commits, using buttons for non-GET requests has
    several advantages over using links.

--- spec/system/admin/proposals_spec.rb
+++ spec/system/admin/proposals_spec.rb
@@ -28,7 +28,7 @@ describe "Admin proposals", :admin do
 
       visit admin_proposals_path
 
-      within("#proposal_#{proposal.id}") { click_link "Select" }
+      within("#proposal_#{proposal.id}") { click_button "Select" }
 
       within("#proposal_#{proposal.id}") { expect(page).to have_link "Selected" }
 
@@ -42,7 +42,7 @@ describe "Admin proposals", :admin do
 
       visit admin_proposals_path
 
-      within("#proposal_#{proposal.id}") { click_link "Selected" }
+      within("#proposal_#{proposal.id}") { click_button "Selected" }
 
       within("#proposal_#{proposal.id}") { expect(page).to have_link "Select" }
 
