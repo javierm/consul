06330a9ea67359af2e8443195b24f64975da267a
Javi Martín <javim@elretirao.net>
2021-08-20 02:26:20 +0200

    Simplify method to get the toggle selection path

--- config/initializers/routes_hierarchy.rb
+++ config/initializers/routes_hierarchy.rb
@@ -24,8 +24,9 @@ module ActionDispatch::Routing::UrlFor
   end
 
   def namespaced_polymorphic_path(namespace, resource, options = {})
-    if %w[Budget::Group Budget::Heading Poll::Booth Poll::BoothAssignment Poll::Officer
-          Poll::Question Poll::Question::Answer::Video Poll::Shift
+    if %w[Budget::Group Budget::Heading Legislation::Proposal Poll::Booth
+          Poll::BoothAssignment Poll::Officer Poll::Question
+          Poll::Question::Answer::Video Poll::Shift
           SDG::LocalTarget].include?(resource.class.name)
       resolve = resolve_for(resource)
       resolve_options = resolve.pop
