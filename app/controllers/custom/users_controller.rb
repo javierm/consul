ef922f3e039844031ec51c41e196f11a29c1eaac
Javi Martín <javim@elretirao.net>
2021-08-24 15:25:35 +0200

    Don't allow users to see what other users follow
    It could be argued that seeing which proposals a user follows is a good
    indicator of which proposals a user has supported.

---
+++ app/controllers/custom/users_controller.rb
@@ -0,0 +1,17 @@
+require_dependency Rails.root.join("app", "controllers", "users_controller").to_s
+
+class UsersController
+  alias_method :consul_show, :show
+
+  def show
+    unless @user == current_user
+      @valid_filters = @valid_filters.reject { |filter| filter.to_s == "follows" }
+    end
+
+    if params[:filter] == "follows" && @user != current_user
+      raise ActionController::RoutingError, "Not Found"
+    else
+      consul_show
+    end
+  end
+end
