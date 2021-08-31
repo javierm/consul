8d18ad242ae1c282077aed933d10ce36e7370005
Michael <info@michaelnieuwenweg.nl>
2021-08-31 13:30:26 +0200

    Block user profile

---
+++ app/controllers/custom/users_controller.rb
@@ -0,0 +1,13 @@
+require_dependency Rails.root.join("app", "controllers", "users_controller").to_s 
+
+class UsersController
+  alias_method :consul_show, :show
+  before_action :authenticate_user!
+
+  def show
+    if @user != current_user
+      raise ActionController::RoutingError, "Not Found"
+    else
+      consul_show
+    end
+  end end
