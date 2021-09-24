01f0b589a04bbb4c37877066acdf80e8345a2b90
Alejandro <alejandro.vera1988@gmail.com>
2021-09-24 12:54:46 +0100

    Fixed local error and  file name.

--- app/helpers/custom/budgets_helper.rb
+++ app/helpers/custom/budgets_helper.rb
@@ -2,7 +2,7 @@ module Custom::BudgetsHelper
   def heading_name_and_price_html(heading, budget)
     tag.div do
       concat(heading.name + " ")
-      concat("(" + heading.min_supports.to_s + " apoyos" + ")" + " ") if heading.min_supports
+      concat("(" + t("budgets.investments.investment.supports", count: heading.min_supports) + ")" + " ") if heading.min_supports
       concat(tag.span(budget.formatted_heading_price(heading)))
     end
   end
