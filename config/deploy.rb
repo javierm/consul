afe62e1d9c0f04c1ac5e0204e9e1fd2da4999115
Ignacio Aliende García <ialiendeg@gmail.com>
2021-09-27 11:35:19 +0100

    Disables some deploy options

--- config/deploy.rb
+++ config/deploy.rb
@@ -47,20 +47,20 @@ set :whenever_roles, -> { :app }
 
 namespace :deploy do
   Rake::Task["delayed_job:default"].clear_actions
-  Rake::Task["puma:smart_restart"].clear_actions
+  # Rake::Task["puma:smart_restart"].clear_actions
 
-  after :updating, "rvm1:install:rvm"
-  after :updating, "rvm1:install:ruby"
+  # after :updating, "rvm1:install:rvm"
+  # after :updating, "rvm1:install:ruby"
   after :updating, "install_bundler_gem"
 
   after "deploy:migrate", "add_new_settings"
 
-  after  :publishing, "setup_puma"
+  # after  :publishing, "setup_puma"
 
   after :published, "deploy:restart"
-  before "deploy:restart", "puma:restart"
+  # before "deploy:restart", "puma:restart"
   before "deploy:restart", "delayed_job:restart"
-  before "deploy:restart", "puma:start"
+  # before "deploy:restart", "puma:start"
 
   after :finished, "refresh_sitemap"
 
