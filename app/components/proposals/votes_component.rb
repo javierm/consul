646d13358db0af0d5896a64898c54ee223a91965
Javi Martín <javim@elretirao.net>
2021-09-28 20:02:41 +0200

    Use the same view for feature and proposal votes
    They were almost identical, with the supports progress and the HTML
    classes of the success message element being the only exceptions.
    
    This change comes at the cost of making the component a bit more
    complex; we'll remove this complexity in a couple of commits.

--- app/components/proposals/votes_component.rb
+++ app/components/proposals/votes_component.rb
@@ -1,9 +1,11 @@
 class Proposals::VotesComponent < ApplicationComponent
-  attr_reader :proposal, :vote_url
+  attr_reader :proposal, :vote_url, :featured
+  alias_method :featured?, :featured
   delegate :current_user, :link_to_verify_account, :user_signed_in?, to: :helpers
 
-  def initialize(proposal, vote_url:)
+  def initialize(proposal, vote_url:, featured: false)
     @proposal = proposal
     @vote_url = vote_url
+    @featured = featured
   end
 end
