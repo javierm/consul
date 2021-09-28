0bd3ae88400981940b41fff6c578c7f24fc289e9
Javi Martín <javim@elretirao.net>
2021-09-28 22:58:02 +0200

    Use the vote action to vote featured proposals
    So we avoid some duplication. The only difference between them were some
    styles, so we can do that with CSS instead.

--- app/components/proposals/votes_component.rb
+++ app/components/proposals/votes_component.rb
@@ -1,12 +1,10 @@
 class Proposals::VotesComponent < ApplicationComponent
-  attr_reader :proposal, :featured
-  alias_method :featured?, :featured
+  attr_reader :proposal
   delegate :current_user, :link_to_verify_account, :user_signed_in?, to: :helpers
 
-  def initialize(proposal, vote_url: nil, featured: false)
+  def initialize(proposal, vote_url: nil)
     @proposal = proposal
     @vote_url = vote_url
-    @featured = featured
   end
 
   def vote_url
