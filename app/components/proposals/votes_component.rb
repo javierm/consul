4cfe070972e2387c81ef37d673a064ab05727385
Javi Martín <javim@elretirao.net>
2021-09-28 23:53:18 +0200

    Simplify passing the proposal vote URL
    In most cases we can use the public one.

--- app/components/proposals/votes_component.rb
+++ app/components/proposals/votes_component.rb
@@ -1,11 +1,15 @@
 class Proposals::VotesComponent < ApplicationComponent
-  attr_reader :proposal, :vote_url, :featured
+  attr_reader :proposal, :featured
   alias_method :featured?, :featured
   delegate :current_user, :link_to_verify_account, :user_signed_in?, to: :helpers
 
-  def initialize(proposal, vote_url:, featured: false)
+  def initialize(proposal, vote_url: nil, featured: false)
     @proposal = proposal
     @vote_url = vote_url
     @featured = featured
+  end
+
+  def vote_url
+    @vote_url || vote_proposal_path(proposal, value: "yes")
   end
 end
