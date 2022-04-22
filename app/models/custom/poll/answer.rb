rockandror/pro-ethics-consul

Add open_answer to poll answers
Consider questions without predefined answers as questions with open answer.

app/models/custom/poll/answer.rb
@@ -3,8 +3,12 @@ require_dependency Rails.root.join("app", "models", "poll", "answer").to_s
 class Poll::Answer
   belongs_to :answer, class_name: "Poll::Question::Answer", inverse_of: :answers
 
+  skip_validation :answer, :presence
   skip_validation :answer, :inclusion
 
-  validates :answer, inclusion: { in: ->(a) { a.question.question_answers }},
-                     unless: ->(a) { a.question.blank? }
+  validates :answer_id, presence: true, if: ->(a) { a.question&.single_choice? }
+  validates :answer_id, inclusion: { in: ->(a) { a.question.question_answers.ids }},
+                        if: ->(a) { a.question&.single_choice? }
+
+  validates :open_answer, presence: true, unless: ->(a) { a.question&.single_choice? }
 end

javierm/consul

app/models/custom/poll/answer.rb
@@ -3,8 +3,12 @@ require_dependency Rails.root.join("app", "models", "poll", "answer").to_s
 class Poll::Answer
   belongs_to :answer, class_name: "Poll::Question::Answer", inverse_of: :answers
 
+  skip_validation :answer, :presence
   skip_validation :answer, :inclusion
 
-  validates :answer, inclusion: { in: ->(a) { a.question.question_answers }},
-                     unless: ->(a) { a.question.blank? }
+  validates :answer_id, presence: true, if: ->(a) { a.question&.is_single_choice? }
+  validates :answer_id, inclusion: { in: ->(a) { a.question.question_answers.ids }},
+                        if: ->(a) { a.question&.is_single_choice? }
+
+  validates :open_answer, presence: true, unless: ->(a) { a.question&.is_single_choice? }
 end