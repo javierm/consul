84b01e563832852f40f512397ce074440a9c55e7
Javi Martín <javim@elretirao.net>
2023-03-07 17:47:05 +0100

    Use .tap

--- app/models/map_location.rb
+++ app/models/map_location.rb
@@ -18,10 +18,10 @@ class MapLocation < ApplicationRecord
   end
 
   def self.load_from_heading(heading)
-    map = new
-    map.zoom = Budget::Heading::OSM_DISTRICT_LEVEL_ZOOM
-    map.latitude = heading.latitude.to_f if heading.latitude.present?
-    map.longitude = heading.longitude.to_f if heading.longitude.present?
-    map
+    new.tap do |map|
+      map.zoom = Budget::Heading::OSM_DISTRICT_LEVEL_ZOOM
+      map.latitude = heading.latitude.to_f if heading.latitude.present?
+      map.longitude = heading.longitude.to_f if heading.longitude.present?
+    end
   end
 end
