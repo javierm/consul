4635da85b702b9e9e0f84322361ef65c71487549
Javi Martín <javim@elretirao.net>
2024-09-12 17:42:50 +0200

    Rename field_name helper to audit_field_name
    Rails 7.0, which is used in Consul since commit 8596f1539 includes a
    `field_name` helper and, since helpers are basically global methods and
    we had a `field_name` method in one helper, we were overwriting the one
    included in Rails.
    
    So we're renaming it in order to avoid the conflict.

--- app/components/custom/admin/audits/show_component.rb
+++ app/components/custom/admin/audits/show_component.rb
@@ -1,7 +1,7 @@
 class Admin::Audits::ShowComponent < ApplicationComponent
   attr_reader :audit
 
-  use_helpers :audit_value, :field_name, :new_value, :old_value, :sanitize, :wysiwyg
+  use_helpers :audit_value, :audit_field_name, :new_value, :old_value, :sanitize, :wysiwyg
 
   def initialize(audit)
     @audit = audit
