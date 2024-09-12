02f266fc70a07e2842e2c5650ccd4c5b349fb76e
Javi Martín <javim@elretirao.net>
2024-09-12 17:55:52 +0200

    Fix custom locales not taking precedence
    This change is currently in the master branch of Consul and will be
    included in Consul 2.2.1. Quoting from the commit message there:
    
    When we upgraded to Rails 7 in commit 8596f1539, we broke the custom
    locales since now all nested folders in `config/locales/` are loaded by
    default [1]. This meant that the custom folder was now loaded before any
    languages whose code alphabetically goes after the word "custom".
    
    As a workaround, we're overwriting the default locales paths so they
    don't include the custom folder. We're doing this step before the
    `add_locales` initializer; that is, before the default locales paths are
    used.
    
    Unfortunately, I haven't found a way to add a test for this behavior,
    since we would need to add a file in `config/locales/custom` that
    overwrites an internationalization key for an existing language, but
    only during a specific test, and the i18n load path is evaluated when
    the application starts up.
    
    [1] See pull request 41872 in https://github.com/rails/rails/

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -9,5 +9,11 @@ module Consul
     config.time_zone = "Atlantic/Canary"
 
     config.autoload_paths << "#{Rails.root}/app/controllers/custom/concerns"
+
+    # TODO: Remove after upgrading to Consul Democracy 2.2.1 or later,
+    # since that version already includes this code
+    initializer :exclude_custom_locales_automatic_loading, before: :add_locales do
+      paths.add "config/locales", glob: "**[^custom]*/*.{rb,yml}"
+    end
   end
 end
