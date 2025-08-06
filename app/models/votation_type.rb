5cfd924066770b9f79b34e0a25a278ecf1afe6ec
taitus <sebastia.roig@gmail.com>
2025-08-06 12:13:13 +0200

    Add validations for changing votation type

--- app/models/votation_type.rb
+++ app/models/votation_type.rb
@@ -1,6 +1,8 @@
 class VotationType < ApplicationRecord
   belongs_to :questionable, polymorphic: true
 
+  validate :cannot_be_essay_if_question_has_options
+
   QUESTIONABLE_TYPES = %w[Poll::Question].freeze
 
   enum :vote_type, { unique: 0, multiple: 1, essay: 2 }
@@ -17,5 +19,11 @@ class VotationType < ApplicationRecord
 
     def max_votes_required?
       multiple?
+    end
+
+    def cannot_be_essay_if_question_has_options
+      if questionable&.question_options&.any? && !accepts_options?
+        errors.add(:vote_type, I18n.t("poll_questions.form.change_votation_type"))
+      end
     end
 end
