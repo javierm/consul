consuldemocracy/consuldemocracy

Manage correctly results and stats for open answers questions

app/components/polls/results/question_component.rb
@@ -12,4 +12,32 @@ class Polls::Results::QuestionComponent < ApplicationComponent
   def most_voted_option?(option)
     question.most_voted_option_ids.include?(option.id)
   end
+
+  def essay_total_responses
+    essay_answered_count + essay_unanswered_count
+  end
+
+  def essay_answered_count
+    @essay_answered_count ||= question.answers.where.not(text_answer: [nil, ""]).count
+  end
+
+  def essay_unanswered_count
+    valid_participants_count - essay_answered_count
+  end
+
+  def essay_answered_percentage
+    return 0.0 if essay_total_responses.zero?
+
+    (essay_answered_count * 100.0) / essay_total_responses
+  end
+
+  def essay_unanswered_percentage
+    return 0.0 if essay_total_responses.zero?
+
+    (essay_unanswered_count * 100.0) / essay_total_responses
+  end
+
+  def valid_participants_count
+    @valid_participants_count ||= question.poll.answers.distinct.count(:author_id)
+  end
 end

spec/components/polls/results/question_component_spec.rb
@@ -2,21 +2,69 @@ require "rails_helper"
 
 describe Polls::Results::QuestionComponent do
   let(:poll) { create(:poll) }
-  let(:question) { create(:poll_question, poll: poll) }
-  let(:option_yes) { create(:poll_question_option, question: question, title: "Yes") }
-  let(:option_no) { create(:poll_question_option, question: question, title: "No") }
 
-  it "renders results table content" do
-    create(:poll_answer, question: question, option: option_yes)
-    create(:poll_answer, question: question, option: option_no)
+  context "question is not essay" do
+    let(:question) { create(:poll_question, poll: poll) }
+    let(:option_yes) { create(:poll_question_option, question: question, title: "Yes") }
+    let(:option_no) { create(:poll_question_option, question: question, title: "No") }
 
-    render_inline Polls::Results::QuestionComponent.new(question)
+    it "renders results table content" do
+      create(:poll_answer, question: question, option: option_yes)
+      create(:poll_answer, question: question, option: option_no)
 
-    page.find("#question_#{question.id}_results_table") do |table|
-      expect(table).to have_css "#option_#{option_yes.id}_result", text: "1 (50.0%)", normalize_ws: true
-      expect(table).to have_css "#option_#{option_no.id}_result", text: "1 (50.0%)", normalize_ws: true
-      expect(table).to have_css "th.win", count: 2
-      expect(table).to have_css "td.win", count: 2
+      render_inline Polls::Results::QuestionComponent.new(question)
+
+      page.find("#question_#{question.id}_results_table") do |table|
+        expect(table).to have_css "#option_#{option_yes.id}_result", text: "1 (50.0%)", normalize_ws: true
+        expect(table).to have_css "#option_#{option_no.id}_result", text: "1 (50.0%)", normalize_ws: true
+        expect(table).to have_css "th.win", count: 2
+        expect(table).to have_css "td.win", count: 2
+      end
+    end
+  end
+
+  context "question is essay" do
+    let(:essay_question) { create(:poll_question_essay, poll: poll) }
+
+    it "renders essay headers and empty counts when there are no participants" do
+      render_inline Polls::Results::QuestionComponent.new(essay_question)
+
+      page.find("#question_#{essay_question.id}_results_table") do |table|
+        expect(table).to have_css "th", text: "With answer"
+        expect(table).to have_css "th", text: "Without answer"
+        with_answer    = "#question_#{essay_question.id}_with_answer_result"
+        without_answer = "#question_#{essay_question.id}_without_answer_result"
+        expect(table).to have_css with_answer, text: "0 (0.0%)", normalize_ws: true
+        expect(table).to have_css without_answer, text: "0 (0.0%)", normalize_ws: true
+      end
+    end
+
+    it "render counts for answered/unanswered among valid poll participants" do
+      other_question = create(:poll_question, :yes_no, poll: poll)
+      option_yes = other_question.question_options.find_by(title: "Yes")
+
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
+      user_4 = create(:user)
+
+      create(:poll_answer, question: other_question, author: user_1, option: option_yes)
+      create(:poll_answer, question: other_question, author: user_2, option: option_yes)
+      create(:poll_answer, question: other_question, author: user_3, option: option_yes)
+      create(:poll_answer, question: other_question, author: user_4, option: option_yes)
+
+      create(:poll_answer, question: essay_question, author: user_1, text_answer: "Open text A")
+      create(:poll_answer, question: essay_question, author: user_2, text_answer: "Open text B")
+      create(:poll_answer, question: essay_question, author: user_3, text_answer: "Open text C")
+
+      render_inline Polls::Results::QuestionComponent.new(essay_question)
+
+      page.find("#question_#{essay_question.id}_results_table") do |table|
+        with_answer    = "#question_#{essay_question.id}_with_answer_result"
+        without_answer = "#question_#{essay_question.id}_without_answer_result"
+        expect(table).to have_css with_answer, text: "3 (75.0%)", normalize_ws: true
+        expect(table).to have_css without_answer, text: "1 (25.0%)", normalize_ws: true
+      end
     end
   end
 end

javierm/consul

app/components/polls/results/question_component.rb
@@ -12,4 +12,32 @@ class Polls::Results::QuestionComponent < ApplicationComponent
   def most_voted_option?(option)
     option.id == question.most_voted_option_id
   end
+
+  def essay_total_responses
+    essay_answered_count + essay_unanswered_count
+  end
+
+  def essay_answered_count
+    @essay_answered_count ||= question.answers.where.not(answer: [nil, ""]).count
+  end
+
+  def essay_unanswered_count
+    valid_participants_count - essay_answered_count
+  end
+
+  def essay_answered_percentage
+    return 0.0 if essay_total_responses.zero?
+
+    (essay_answered_count * 100.0) / essay_total_responses
+  end
+
+  def essay_unanswered_percentage
+    return 0.0 if essay_total_responses.zero?
+
+    (essay_unanswered_count * 100.0) / essay_total_responses
+  end
+
+  def valid_participants_count
+    @valid_participants_count ||= question.poll.answers.distinct.count(:author_id)
+  end
 end

spec/components/polls/results/question_component_spec.rb
@@ -1,0 +1,70 @@
+require "rails_helper"
+
+describe Polls::Results::QuestionComponent do
+  let(:poll) { create(:poll) }
+
+  context "question is not essay" do
+    let(:question) { create(:poll_question, poll: poll) }
+    let(:option_yes) { create(:poll_question_option, question: question, title: "Yes") }
+    let(:option_no) { create(:poll_question_option, question: question, title: "No") }
+
+    it "renders results table content" do
+      create(:poll_answer, question: question, option: option_yes)
+      create(:poll_answer, question: question, option: option_no)
+
+      render_inline Polls::Results::QuestionComponent.new(question)
+
+      page.find("#question_#{question.id}_results_table") do |table|
+        expect(table).to have_css "#option_#{option_yes.id}_result", text: "1 (50.0%)", normalize_ws: true
+        expect(table).to have_css "#option_#{option_no.id}_result", text: "1 (50.0%)", normalize_ws: true
+        expect(table).to have_css "th.win", count: 2
+        expect(table).to have_css "td.win", count: 2
+      end
+    end
+  end
+
+  context "question is essay" do
+    let(:essay_question) { create(:poll_question_essay, poll: poll) }
+
+    it "renders essay headers and empty counts when there are no participants" do
+      render_inline Polls::Results::QuestionComponent.new(essay_question)
+
+      page.find("#question_#{essay_question.id}_results_table") do |table|
+        expect(table).to have_css "th", text: "With answer"
+        expect(table).to have_css "th", text: "Blank"
+        with_answer    = "#question_#{essay_question.id}_with_answer_result"
+        without_answer = "#question_#{essay_question.id}_without_answer_result"
+        expect(table).to have_css with_answer, text: "0 (0.0%)", normalize_ws: true
+        expect(table).to have_css without_answer, text: "0 (0.0%)", normalize_ws: true
+      end
+    end
+
+    it "render counts for answered/unanswered among valid poll participants" do
+      other_question = create(:poll_question, :yes_no, poll: poll)
+      option_yes = other_question.question_options.find_by(title: "Yes")
+
+      user_1 = create(:user)
+      user_2 = create(:user)
+      user_3 = create(:user)
+      user_4 = create(:user)
+
+      create(:poll_answer, question: other_question, author: user_1, option: option_yes)
+      create(:poll_answer, question: other_question, author: user_2, option: option_yes)
+      create(:poll_answer, question: other_question, author: user_3, option: option_yes)
+      create(:poll_answer, question: other_question, author: user_4, option: option_yes)
+
+      create(:poll_answer, question: essay_question, author: user_1, answer: "Open text A")
+      create(:poll_answer, question: essay_question, author: user_2, answer: "Open text B")
+      create(:poll_answer, question: essay_question, author: user_3, answer: "Open text C")
+
+      render_inline Polls::Results::QuestionComponent.new(essay_question)
+
+      page.find("#question_#{essay_question.id}_results_table") do |table|
+        with_answer    = "#question_#{essay_question.id}_with_answer_result"
+        without_answer = "#question_#{essay_question.id}_without_answer_result"
+        expect(table).to have_css with_answer, text: "3 (75.0%)", normalize_ws: true
+        expect(table).to have_css without_answer, text: "1 (25.0%)", normalize_ws: true
+      end
+    end
+  end
+end